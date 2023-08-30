a857664a8aca1df7bb60f93ed71641abc5efb594
Nick <129078463+nicholasjmorris1993@users.noreply.github.com>
2023-08-30 14:34:24 -0400

    Add files via upload

--- test/test_inventory.py
+++ test/test_inventory.py
@@ -1,1 +1,11 @@
+import pandas as pd
+import sys
+sys.path.append("/home/nick/QR-Model/src")
+from inventory import qr
 
+
+data = pd.read_csv("/home/nick/QR-Model/test/inventory.csv")
+
+model = qr(data)
+print("\n")
+print(model.data)
