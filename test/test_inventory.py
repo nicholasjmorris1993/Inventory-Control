8c86c722bad3b09b6266dd9340dd276d372fd2d8
Nick <129078463+nicholasjmorris1993@users.noreply.github.com>
2023-09-01 16:04:39 -0400

    Add files via upload

--- test/test_inventory.py
+++ test/test_inventory.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import sys
-sys.path.append("/home/nick/QR-Model/src")
+sys.path.append("/home/nick/Inventory-Control/src")
 from inventory import qr
 
 
-data = pd.read_csv("/home/nick/QR-Model/test/inventory.csv")
+data = pd.read_csv("/home/nick/Inventory-Control/test/inventory.csv")
+products = pd.unique(data["Product"])
+models = dict()
 
-model = qr(data)
-print("\n")
-print(model.data)
+for p in products:
+    df = data.copy().loc[data["Product"] == p].drop(columns="Product").reset_index(drop=True)
+
+    print(f"\n---- {p} ----\n")
+    models[p] = qr(df, name=p)
+    print("\nInventory Policy:")
+    print(models[p].data)
